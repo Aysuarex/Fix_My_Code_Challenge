84136736bcfad8a1bbb504265ac80e9479980234
Aysuarex <aysuarex@gmail.com>
2022-10-09 21:41:13 +0100

    Updated Square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 class square():
+    
     width = 0
     height = 0
 
+    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -17,7 +19,6 @@ class square():
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
